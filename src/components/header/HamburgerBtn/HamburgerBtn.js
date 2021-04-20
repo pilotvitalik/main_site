e5e0c04273abb4a10c343b00eb75bd33b72ec1e3
Vitaliy <itvkip@yandex.ru>
2021-04-20 22:45:59 +0300

    Навбар (добавление пункта NodeJS): - при клике на кнопку гамбургер, кнопка превращается в крестик; - добавлен функционал для отображения/скрытия навбара в мобильной версии;

--- src/components/header/HamburgerBtn/HamburgerBtn.js
+++ src/components/header/HamburgerBtn/HamburgerBtn.js
@@ -1,40 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import MenuIcon from '@material-ui/icons/Menu';
-import { makeStyles } from '@material-ui/core/styles';
+import style from './hamburgerBtn.module.css';
 
 import { useDispatch } from 'react-redux';
 
-const AppStyles = makeStyles((theme) => ({
-  root: {
-    width: '32px',
-    height: '32px',
-    color: '#fff',
-    transition: 'all .3s linear',
-    'margin-left': '7px',
-    'margin-right': 'auto',
-    '@media(min-width: 992px)': {
-      display: 'none',
-    },
-    '&:hover':  {
-      cursor: 'pointer',
-    },
-    '&.rotateSVG': {
-      transform: 'rotate(90deg)',
-    }
-  },
-}));
-
 function HamburgerBtn(props){
-  const classes = AppStyles();
   const dispatch = useDispatch();
+  const [isOpen, changeType] = useState(false);
 
   function showHideNavbar(e){
     dispatch({type: 'header/visibleNavbar', payload: ''});
+    changeType(!isOpen);
   }
 
   return(
-    <MenuIcon className={classes.root} onClick={showHideNavbar}/>
+    <div className={isOpen ? style.menuHamb + ' ' + style.open : style.menuHamb} onClick={showHideNavbar}>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
   );
 }
 
