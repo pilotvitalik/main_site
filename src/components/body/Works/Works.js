317ce58a6568b69ed6de5f5214ab4c6a4104263e
Vitaliy <itvkip@yandex.ru>
2021-04-24 14:49:49 +0300

    Навбар (добавление пункта NodeJS): отображаются вложенные страницы внутри NodeJS;

--- src/components/body/Works/Works.js
+++ src/components/body/Works/Works.js
@@ -2,14 +2,15 @@ import React from 'react';
 import style from './works.module.css';
 import ReverseNavigate from '../ReverseNavigate/ReverseNavigate';
 import { useSelector } from 'react-redux';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, Switch, Route, useLocation, useRouteMatch, useParams } from 'react-router-dom';
 
 function Works(){
 	const categories = useSelector(state => state.main.categories);
 	const location = useLocation();
+	let {path, url} = useRouteMatch();
 	
 	const listCat = categories.map(item => 
-		<Link key={item.id} to={location.pathname + item.url_page} className={style.cat}>
+		<Link key={item.id} to={`${url}${item.url_page}`} className={style.cat}>
 			<img src={process.env.PUBLIC_URL + item.url_image} alt={item.alt}/>
 			<span>{item.name}</span>
 		</Link>
@@ -17,9 +18,14 @@ function Works(){
     return(
         <div className={style.worksWrapper}>
 			<ReverseNavigate/>
-			<div className={style.categories}>
-				{listCat}
-			</div>
+			<Switch>
+				<Route exact path={path}>
+					<div className={style.categories}>{listCat}</div>
+				</Route>
+				<Route path={`${path}/nodejs`}>
+					<div className={style.categories}>Hello world!</div>
+				</Route>
+			</Switch>
         </div>
     )
 }
