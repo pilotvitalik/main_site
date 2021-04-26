32c853748944f03f3613a3c92dc049077ac3c52b
Vitaliy <12345678t>
2021-04-26 17:29:07 +0300

    Страница "/file_to_server": - начато добавления функционала для загрузки файла с помощью перетаскивания;

--- src/components/body/Works/NodeJS/FileToServer/FileToServer.js
+++ src/components/body/Works/NodeJS/FileToServer/FileToServer.js
@@ -3,15 +3,21 @@ import style from './fileToServer.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 
 function FiletoServer(){
+	const eventsArr = ['onDragEnter = {dragDropFile}', 'onDragOver = {dragDropFile}', 'onDragLeave = {dragDropFile}', 'onDrop = {dragDropFile}'];
 	let dispatch = useDispatch();
 	dispatch({type: 'main/nodeUploadFile', payload: ''});
 
 	const images = useSelector(state => state.main.uploadFiles);
-	console.log(images);
+
+	function dragDropFile(e){
+		console.log(e);
+	}
 
 	return(
 		<div className={style.wrapperFiles}>
-			<div className={style.uploadBlock}>
+			<div
+				{...eventsArr, eventsArr.forEach(item => { return item})}
+				className={style.uploadBlock}>
 				<div className={style.dropDownBlock}>
 					<img src={images.drag_drop} alt={images.alt_drag_drop}/>
 					<p>
