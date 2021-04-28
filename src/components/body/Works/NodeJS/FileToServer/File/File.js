757a2d8fe85a050c358357064ff9b5de4a76289c
Vitaliy <12345678t>
2021-04-28 17:29:01 +0300

    Страница "/file_to_server": - добавлена иконка для кнопки удалить загруженный файл;

--- src/components/body/Works/NodeJS/FileToServer/File/File.js
+++ src/components/body/Works/NodeJS/FileToServer/File/File.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import style from '../fileToServer.module.css';
+import Close from '@material-ui/icons/Close';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        color: '#d5d5d5',
+        '&:hover':{
+            cursor: 'pointer',
+            color: '#000',
+        }
+    },
+}));
 
 function File(props){
+    const classes = useStyles();
     const kb = 1024;
     const mb = 1048576;
     let actSize = (props.file.size < 102400) ? `${(props.file.size/kb).toFixed(2)} КБ` : `${(props.file.size/mb).toFixed(2)} МБ`;
@@ -12,10 +25,12 @@ function File(props){
                 <label htmlFor={props.idInput}></label>
                 <input id={props.idInput} type='checkbox'/>
             </form>
-            <p>{props.ind + 1}.</p>
-            <p>{props.file.name.replace(/.[a-zA-Z]+$/g, '')}</p>
-            <p>{actSize}</p>
-            <button type='button'><span></span><span></span></button>
+            <p className={style.number}>{props.ind + 1}.</p>
+            <p className={style.name}>{props.file.name.replace(/.[a-zA-Z]+$/g, '')}</p>
+            <p className={style.size}>{actSize}</p>
+            <div>
+                <Close className={classes.root}/>
+            </div>
         </div>
     );
 }
