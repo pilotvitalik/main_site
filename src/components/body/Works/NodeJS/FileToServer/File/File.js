e05c46a1e65ccbcf27b8c814cf9dc10fdb10d509
Vitaliy <itvkip@yandex.ru>
2021-04-29 21:37:53 +0300

    Страница "/file_to_server": - добавлен функционал добавления галочки в чекбокс при клике;

--- src/components/body/Works/NodeJS/FileToServer/File/File.js
+++ src/components/body/Works/NodeJS/FileToServer/File/File.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import Close from '@material-ui/icons/Close';
+import Mark from '@material-ui/icons/Check';
 import style from '../fileToServer.module.css';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
+    closeBtn: {
         color: '#d5d5d5',
         '&:hover':{
             cursor: 'pointer',
             color: '#000',
         }
     },
+    mark:{
+        position: 'absolute',
+        width: '15px',
+        height: '15px',
+        top: '-1px',
+        left: '-1px',
+    }
 }));
 
 function File(props){
+    const [mark, setMark] = useState(false);
+
     const classes = useStyles();
     const dispatch = useDispatch();
     const kb = 1024;
     const mb = 1048576;
+
     let actSize = (props.file.size < 102400) ? `${(props.file.size/kb).toFixed(2)} КБ` : `${(props.file.size/mb).toFixed(2)} МБ`;
 
     function deleteFile(e){
@@ -29,13 +40,20 @@ function File(props){
     return(
         <div className={style.itemFile}>
             <form>
-                <label htmlFor={props.idInput}></label>
+                <label 
+                    htmlFor={props.idInput}
+                    onClick={() => setMark(!mark)}
+                    className={mark ? style.labelCheked : ''}>
+                    {mark
+                        ? <Mark className={classes.mark}/>
+                        : ''}
+                </label>
                 <input id={props.idInput} type='checkbox'/>
             </form>
             <p className={style.number}>{props.ind + 1}.</p>
             <p className={style.name}>{props.file.name.replace(/.[a-zA-Z]+$/g, '')}</p>
             <p className={style.size}>{actSize}</p>
-            <Close className={classes.root} data-file-id={props.idInput} onClick={deleteFile}/>
+            <Close className={classes.closeBtn} data-file-id={props.idInput} onClick={deleteFile}/>
         </div>
     );
 }
